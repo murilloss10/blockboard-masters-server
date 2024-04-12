6a753794c1bb5a25aea9e9c6685577692bf6241c
Murillo Santos <murillo.santos@cromg.org.br>
2024-04-12 06:47:21 -0300

    feat: added the pieces in the endpoint return

--- app/Http/Controllers/ChessController.php
+++ app/Http/Controllers/ChessController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chess\Bishop;
+use App\Models\Chess\Horse;
+use App\Models\Chess\King;
 use App\Models\Chess\Tower;
 use App\Models\Chess\Pawn;
 use App\Models\Chess\Queen;
@@ -15,13 +17,17 @@ class ChessController extends Controller
         $pawn   = new Pawn();
         $tower  = new Tower();
         $bishop = new Bishop();
-        $queen = new Queen();
+        $queen  = new Queen();
+        $king   = new King();
+        $horse  = new Horse();
 
         return response()->json([
-            'pawn' => $pawn->getMoves(),
-            'tower' => $tower->getMoves(),
-            'bishop' => $bishop->getMoves(),
-            'queen' => $queen->getMoves(),
+            'pawn'      => $pawn->getMoves(),
+            'tower'     => $tower->getMoves(),
+            'bishop'    => $bishop->getMoves(),
+            'queen'     => $queen->getMoves(),
+            'king'      => $king->getMoves(),
+            'horse'     => $horse->getMoves()
         ]);
     }
 }
